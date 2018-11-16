06866d41b879150534ee751481de8655d78847ca
patLoeber <patrickloeber90@googlemail.com>
2018-11-16 13:52:14 +0100

    implemented arraystack

--- data_structures/arraystack.py
+++ data_structures/arraystack.py
@@ -3,5 +3,39 @@ class Empty(Exception):
 
 
 class ArrayStack:
-    pass
+    # LIFO stack implementation using a Python list as underlying storage
 
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def __str__(self):
+        return repr(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('stack is empty')
+        return self._data.pop()
+
+
+a = ArrayStack()
+a.push(1)
+a.push(2)
+print("S = ", a)
+print("top: ", a.top())
+print("stack size: ", len(a))
+print("pop: ", a.pop())
+print("stack size: ", len(a))
